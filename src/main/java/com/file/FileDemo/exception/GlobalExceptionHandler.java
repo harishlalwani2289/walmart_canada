8cd0e46d1e540c6a14de8889f69d7b72ed723655
Harish Lalwani <harishlalwani2289@gmail.com>
2022-09-12 19:22:40 +0530

    Added exception for file size greater than limit

--- src/main/java/com/file/FileDemo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/file/FileDemo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.file.FileDemo.exception;
 
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,19 +9,30 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @Value("${spring.servlet.multipart.max-file-size}")
-    String maxPermittedSize;
+    @Value("${spring.servlet.multipart.max-request-size}")
+    String maxPermittedRequestSize;
 
-    @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<ExceptionMessage> handleMaxUploadException(MaxUploadSizeExceededException e, HttpServletRequest request, HttpServletResponse response){
+    @Value(("${spring.servlet.multipart.max-file-size}"))
+    String maxPermittedFileSize;
+
+    @ExceptionHandler(FileSizeLimitExceededException.class)
+    public ResponseEntity<ExceptionMessage> handleFileSizeLimitExceededException(FileSizeLimitExceededException e, HttpServletResponse response) {
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        return  new ResponseEntity<>(new ExceptionMessage("File " + "" + "size exceeds the permitted value " + maxPermittedSize), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(new ExceptionMessage(" File " + e.getFileName() +
+                " size exceeds the permitted value " + maxPermittedFileSize), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ExceptionMessage> handleMaxUploadException(MaxUploadSizeExceededException e, HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        return new ResponseEntity<>(new ExceptionMessage(" Total File " +
+                " size exceeds the permitted value " + maxPermittedRequestSize), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
